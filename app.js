c4ab202bed5ab4b85ed6c834e987e704340e00f6
Alex Kim <alterprist@gmail.com>
2017-10-30 17:11:52 -0400

    working with excel spreadsheet works, maybe have different offset to incorporate difficulty and few other features

--- app.js
+++ app.js
@@ -12,19 +12,62 @@ function shuffle(array, size){
 }
 
 
-app.get('/quals/:game', function(req, res, next){
+app.get('/:game/:min/:max', function(req, res, next){
   var name = req.params.game;
   var filename = "guac_" + name + ".xlsx";
 
+  //due to this being guac specific, some calls will be very specific to this case
+  //this code, if used elsewhere, would not work as well as it would
+  //i.e. selecting within range + level would require sql type call
+  //unless organized very well
+
+  //yes, this is framework for the 'master' random song generator that will come later
+  //but for now, i have deadline
+
+  //personal note: heroku has 10k limit for each postgres database, but i dont think
+  //there's a limit on how much i can make
+
+  //if i really want to host this for FREE, i need to get through some loopholes
+  //todo later: make mental note somewhere else
   try{
     var workbook = excel.readFile(filename);
   }
-  catch(err){
+  catch(err){0
     console.log("File not found for game " + name);
     res.status(404);
     res.end();
   }
+  var total = 0;
+  var offset = 0;
+  var worksheet = workbook.Sheets[workbook.SheetNames[0]]
+  var x = 1;
+  for(x; x < req.params.min; x++){
+    var cell = "E"+x;
+    offset += worksheet[cell].v;
+  }
+  for(x; x <= req.params.max; x++){
+
+    var cell = "E"+x;
+    total += worksheet[cell].v;
+  }
+
+  var random = Math.floor((Math.random() * total)) + 1 + offset;
+  console.log(random);
+  var songname = worksheet[("A" + random)].v;
+  var artist = worksheet[("B" + random)].v;
+  var level = worksheet[("C" + random)].v;
+
+  console.log(songname);
+  console.log(artist);
+  console.log(level);
+
+  var return_obj = {
+    songname: songname,
+    artist: artist,
+    level: level
+  }
 });
+
 
 //won't be used for guac, but something interesting to think about, especially useful for card-draw
 app.get('/ddr/:count/:min/:max', function(req, res, next){
